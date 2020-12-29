66da6f861128f07cc708e35b76ae4ee01f88322d
mpereira19 <74026152+mpereira19@users.noreply.github.com>
2020-12-29 17:59:10 +0000

    Work Files

---
+++ BatalhaNavalEngine.py
@@ -0,0 +1,79 @@
+# -*- coding:utf-8 -*-
+'''
+Created on 28/10/2019
+
+@author: valves
+'''
+
+
+
+
+class BatalhaNavalEngine:
+    
+    def __init__(self):
+        self.tab_jogo = [] #matriz que representa o tabuleiro de jogo original
+        self.tab_estado = [] #matriz que representa o tabuleiro com o estado do jogo
+        self.jogador= "top_gun"
+        self.score=0
+    
+    def ler_tabuleiro_ficheiro(self, filename):
+        '''
+        Cria nova instancia do jogo numa matriz
+        :param filename: nome do ficheiro a ler
+        como o formato é fixo sabe-se o que contem cada linha
+        '''
+        try:
+            ficheiro = open(filename, "r")
+            lines = ficheiro.readlines() #ler as linhas do ficheiro para a lista lines
+            self.tab_jogo=[]
+            for i in range(1,11): #as linhas 1 a 11 contêm o tabuleiro de jogo
+                self.tab_jogo.append(lines[i].split())
+            self.tab_estado=[]
+            for i in range(12,22):
+                self.tab_estado.append(lines[i].split())
+            estado=True
+            self.score =int(lines[23])
+        except:
+            print("Erro: na leitura do tabuleiro")
+            estado=False
+        else:
+            ficheiro.close()
+        return estado
+    
+    def print_tab_jogo(self):
+        print("  1 2 3 4 5 6 7 8 9 10")
+        letras=['A','B','C','D','E','F','G','H','I','J']
+        i=0
+        for linha in self.tab_jogo:
+            print(letras[i],end=" ")
+            i+=1
+            for simbolo in linha:
+                print(simbolo,end=" ")
+            print()
+        print("[%s] Jogadas efetuadas:%d"%(self.jogador, self.score))
+        
+    def print_tab_estado(self):
+        print("\n  1 2 3 4 5 6 7 8 9 10")
+        letras=['A','B','C','D','E','F','G','H','I','J']
+        i=0
+        for linha in self.tab_estado:
+            print(letras[i],end=" ")
+            i+=1
+            for simbolo in linha:
+                print(simbolo,end=" ")
+            print()
+        print("[%s] Jogadas efetuadas:%d"%(self.jogador, self.score))
+
+    def setjogador(self,jog):
+        self.jogador=jog
+    
+    def getjogador(self):
+        return self.jogador
+    
+
+    
+    def gettab_estado(self):
+        return self.tab_estado
+
+    def settab_estado(self, t):
+        self.tab_estado = t
---
+++ BatalhaNavalShell.py
@@ -0,0 +1,91 @@
+# -*- coding:utf-8 -*-
+'''
+Created on 28/10/2019
+
+@author: valves
+'''
+from cmd import *
+from BatalhaNavalWindow import BatalhaNavalWindow
+from BatalhaNavalEngine import BatalhaNavalEngine
+
+
+class BatalhaNavalShell(Cmd):
+    intro = 'Interpretador de comandos para a Batalha Naval adaptada. Escrever help ou ? para listar os comandos disponíveis.\n'
+    prompt = 'BatalhaNaval> '
+                               
+    def do_jogar(self, arg):
+        " -  comando jogar que leva como parâmetro o nome de um ficheiro e a identificação do jogador e carrega o tabuleiro permitindo jogá-lo..: jogar <nome_ficheiro> <jogador> \n" 
+        try:
+            lista_arg = arg.split()
+            num_args = len(lista_arg)
+            if num_args == 2:
+                eng.ler_tabuleiro_ficheiro(lista_arg[0])
+                eng.setjogador(lista_arg[1])
+                #eng.print_tab_jogo()
+                eng.print_tab_estado()
+            else:
+                print("Número de argumentos inválido!")
+        except:
+            print("Erro: ao mostrar o puzzle")
+    def do_gravar(self, arg):
+        " - comando gravar que leva como parâmetro o nome de um ficheiro e permite gravar o estado do jogo atual..: gravar <nome_ficheiro>  \n"
+        pass
+    
+    def do_tiro(self, arg):    
+        " - comando tiro que leva como parâmetros a linha e a coluna de uma casa onde se pretende jogar..: tiro <l> <c>\n"
+        pass      
+    def do_agua(self, arg):    
+        " - comando que leva como parâmetros a linha e a coluna de uma casa, pertencente a uma embarcação já afundada (totalmente descoberta) que se pretende rodear de “água”..: agua <l> <c> \n"
+        pass
+    def do_linha(self, arg):    
+        " - comando linha que permite colocar o estado de todas as casas da linha l que ainda não estão determinadas como sendo “água”...: linha <l> \n"
+        pass    
+    def do_coluna(self, arg):    
+        " - comando coluna que permite colocar o estado de todas as casas da coluna c que ainda não estão determinadas como sendo “água”...: coluna <c> \n"
+        pass        
+    def do_ajuda(self, arg):    
+        " - comando ajuda que indica por linha e por coluna a quantidade de segmentos de barco existentes nessa linha/coluna..: ajuda  \n"
+        pass
+    
+    def do_undo(self, arg):    
+        " - comando para anular movimentos (retroceder no jogo): undo \n"
+        pass
+    
+    def do_bot(self, arg):    
+        " - comando bot para apresentar a sequência de jogadas ótimas para terminar o jogo: bot \n"
+        pass
+
+    def do_gerar(self, arg):    
+        " - comando gerar que gera tabuleiros validos..: gerar \n"
+        pass
+    def do_score(self, arg):    
+        " - comando score que permite ver o registo ordenado dos scores dos jogadores..: \n"
+        pass
+    def do_ver(self, arg):    
+        " - Comando para visualizar o estado atual do tabuleiro em ambiente grafico caso seja válido: VER  \n"
+        global janela  # pois pretendo atribuir um valor a um identificador global
+        if janela is not None:
+            del janela  # invoca o metodo destruidor de instancia __del__()
+        janela = BatalhaNavalWindow(40) 
+        janela.mostraJanela(eng.gettab_estado())        
+        
+    def do_sair(self, arg):
+        "Sair do programa BatalhaNaval: sair"
+        print('Obrigado por ter utilizado o BatalhaNaval, espero que tenha sido divertido!')
+        global janela  # pois pretendo atribuir um valor a um identificador global
+        if janela is not None:
+                    del janela  # invoca o metodo destruidor de instancia __del__()
+        return True
+
+
+if __name__ == '__main__':
+    eng = BatalhaNavalEngine()
+    janela = None
+    sh = BatalhaNavalShell()
+    sh.cmdloop()
+    
+'''
+
+
+'''
+
